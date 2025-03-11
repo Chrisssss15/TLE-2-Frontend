2088c5d53ab6b83f0e035e2435bf0eb6f2127c07
J <junocraane@gmail.com>
2025-03-11 13:54:02 +0100

    added grammar and goign to add flshcards

--- src/Flashcards.jsx
+++ src/Flashcards.jsx
@@ -1,82 +1,114 @@
+import { useEffect, useState } from "react";
 
-const words = [
+const questions = [
     {
-        question: "Naam",
-        answer: "image here"
+        question: "Doel",
+        answer: "VIDEO"
     },
     {
-        question: "Leeftijd",
-        answer: "image here"
+        question: "Ervaring",
+        answer: "VIDEO"
     },
     {
-        question: "Werk",
-        answer: "image here"
+        question: "Motivatie",
+        answer: "VIDEO"
     },
     {
-        question: "Hobby",
-        answer: "image here"
+        question: "Achtergrond",
+        answer: "VIDEO"
     },
     {
-        question: "Gezin",
-        answer: "image here"
-    },
-    {
-        question: "Woonplaats",
-        answer: "image here"
-    },
-    {
-        question: "Taal",
-        answer: "image here"
-    },
-    {
-        question: "Gezondheid",
-        answer: "image here"
+        question: "Verwachtingen",
+        answer: "VIDEO"
     }
 ];
 
-let currentQuestionIndex = -1;
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("DOM fully loaded and parsed");
-    currentQuestionIndex = -1;
-    let startButton = document.getElementById("startButton");
-    startButton.addEventListener("click", () =>setupQuestion(startButton));
-
-});
-
-function setupQuestion(startButton){
-
-    // skrt || indicator for what question you are, later for what question inside quiz
-    currentQuestionIndex++;
-
-    console.log("set up questions function reached");
-    startButton.remove();
-
-    let question = document.createElement("h2");
-    question.innerHTML = questions[currentQuestionIndex].question;
-    console.log('question', question, words[currentQuestionIndex]);
-    question.className = "text-3xl font-bold text-gray-800 text-center mt-4 mb-19";
-    document.getElementById("canvas").appendChild(question);
 
 
-}
+
 
 function Flashcards(){
+    const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+    const [userAnswer, setUserAnswer] = useState("");
+    const [started, setStarted] = useState(false);
+    const [flipped, setFlipped] = useState(false);
 
+
+
+    function startGrammar(){
+        setStarted(true);
+
+
+        console.log(currentQuestionIndex);
+        console.log([questions[currentQuestionIndex].question]);
+
+
+    }
+
+    function checkAnswer(){
+
+
+        let correctAnswer = questions[currentQuestionIndex].answer;
+        console.log(correctAnswer);
+        console.log(userAnswer);
+        //str = str.replace(/\s+/g, '');
+        let spacelessAnswer = userAnswer.replace(/\s+/g, '');
+        console.log(userAnswer);
+
+        if (correctAnswer === userAnswer || correctAnswer === spacelessAnswer){
+            console.log("Correct");
+
+            setCurrentQuestionIndex(currentQuestionIndex + 1);
+        };
+        setUserAnswer("");
+    }
     return(
         <>
 
 
             <p>Hier komt de contact informatie</p>
+            {started === false ? (
+                <div id="canvas" className="flex flex-col items-center justify-center h-screen">
+                    <button
+                        onClick={startGrammar}
+                        id="startButton" className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-8 px-16 rounded-lg shadow-lg transition duration-300 ease-in-out">
+                        Start Quiz
+                    </button>
+                </div>
+            ) : (
 
-            <div id="canvas" className="flex flex-col items-center justify-center h-screen">
-                <button id="startButton" className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-8 px-16 rounded-lg shadow-lg transition duration-300 ease-in-out">
-                    Start Flashcards Oefening
-                </button>
-            </div>
 
+
+
+                <div className="flex items-center justify-center h-screen w-full text-center">
+
+
+                    <button onClick={() => setCurrentQuestionIndex(currentQuestionIndex + 1)} className="w-12 h-12 bg-red-600">Fout</button>
+                    <div className="flex flex-col items-center justify-center h-screen w-80 text-center">
+                    {flipped === false ? (
+
+
+                    <h2 className="font-bold text-3xl mb-16">{[questions[currentQuestionIndex].question]}</h2>
+                ) : (
+                    <h2 className="font-bold text-3xl mb-16">{[questions[currentQuestionIndex].answer]}</h2>
+                    )}
+                    <button
+                        onClick={() => setFlipped(!flipped)}
+                        className="bg-blue-700 text-white"
+                    >Draai Kaartje</button>
+                    </div>
+                    <button className="w-12 h-12 bg-green-500">Goed</button>
+                </div>
+
+
+
+
+            )}
 
 
         </>
     )
 }
-export default Flashcards
+export default Flashcards;
+
+
