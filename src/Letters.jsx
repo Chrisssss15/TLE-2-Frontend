301d0dcb84fe3fd0f3ac97c09e348aeea8f5176e
jerdenella <jerdenella@gmail.com>
2025-03-11 14:25:46 +0100

    fetch test

---
+++ src/Letters.jsx
@@ -0,0 +1,22 @@
+// import React, { useState } from "react";
+
+function Letter({ sign }) {
+    return (
+        <li className="bg-white p-4 rounded-lg shadow-lg text-center">
+            <h3 className="text-lg font-semibold mb-2">{sign.definition}</h3>
+            {sign.video_path.match(/\.(mp4|webm|ogg)$/i) ? (
+                <video width="200" controls className="mx-auto">
+                    <source src={sign.video_path} type="video/mp4"/>
+                    Je browser ondersteunt geen video-element.
+                </video>
+            ) : (
+                <img src={sign.video_path} alt={sign.definition} width="200" className="mx-auto"/>
+            )}
+        </li>
+    );
+}
+export default Letter;
+
+
+
+
